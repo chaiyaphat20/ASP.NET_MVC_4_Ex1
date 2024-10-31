71310454a32a3e6fb9f6dd183edc0490be5f746d
chaiyaphat supharak <chaiyaphat2024@gmail.com>
2024-10-31 11:09:01 +0700

    add

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController()
+        {
+            customers = cache["customer"] as List<Customer>;
+            if(customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["customer"] = customers;
+        }
+
+        public ActionResult Index() //Defualt View
+        {
+            Debug.WriteLine("ART");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.MyProperty = "MyProperty";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer==null)
+            {
+                return HttpNotFound();
+            }
+             return View(customer);
+        }
+
+
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        public ActionResult EditCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult EditCustomer(Customer customer,string id)
+        {
+            Customer customerToEdit = customers.FirstOrDefault(c => c.Id == id);
+            if (customerToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                customerToEdit.Name = customer.Name;         
+                customerToEdit.Telephone = customer.Telephone;
+                SaveCache();
+                return RedirectToAction("CustomerList");
+            }
+            
+        }
+
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer); //กรอกไม่ผ่านให้อยู่หน้าเดิม
+            }
+            customer.Id = Guid.NewGuid().ToString();
+            Debug.WriteLine("customer", customer);
+            customers.Add(customer);
+            SaveCache();
+            return RedirectToAction("CustomerList");
+        }
+
+        public ActionResult CustomerList()
+        {
+            return View(customers);
+        }
+    }
+} 
---
+++ HelloMVC/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class Customer
+    {
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "กรุณากรอกชื่อ")]
+        [StringLength(10, ErrorMessage = "ห้ามเกิน 10 ตัว")]
+        [DisplayName("Enter your name")]
+        public string Name { get; set; }
+
+
+        public string Telephone { get; set; }
+    }
+}
